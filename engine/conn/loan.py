05138fb7d1b775ed6d5c19e6a89ef6ee13094f87
root <root@web-server-bv.northamerica-northeast1-a.c.bet-vantage.internal>
2021-07-15 01:19:35 +0300

    5th commit

--- engine/conn/loan.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-class Loan():
-    def __init__(self,amount,msisdn,collateral_size,has_collateral):
-        self.amount = amount
-        self.msisdn = msisdn
-        self.collateral_size = collateral_size
-        self.has_collateral = has_collateral
